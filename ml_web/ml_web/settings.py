c14b67d9f91cb3717fd228506c66403e1272f5ba
debrup416 <debrup416@gmail.com>
2022-01-05 20:45:43 +0530

    crispy added

--- ml_web/ml_web/settings.py
+++ ml_web/ml_web/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
