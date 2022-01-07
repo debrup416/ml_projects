7a25769de9241d64b7a86549f564656423edee0c
debrup416 <debrup416@gmail.com>
2022-01-07 21:37:56 +0530

    User profile added

--- ml_web/ui/apps.py
+++ ml_web/ui/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ui'
+
+    def ready(self):
+        import ui.signals
+        
+
