af97d7ee3223d40101f09445cb8d21a175b9829c
debrup416 <debrup416@gmail.com>
2022-02-01 17:10:47 +0530

    .

--- ml_web/ml_web/settings.py
+++ ml_web/ml_web/settings.py
@@ -24,9 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG'))=='1'
 
 ALLOWED_HOSTS = []
+
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
+    
+
 
 
 # Application definition
